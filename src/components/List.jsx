874b55cd082e60b3d673edc57aa2fdf9236c82c8
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-04-22 21:23:47 +0300

    Added messages for empty lists.
    boy, am I funny

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -34,7 +34,65 @@ const TitleDiv = styled.div`
 
 const CardsDiv = styled.div``;
 
+const EmptyDiv = styled.div`
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+  color: grey;
+`;
+
 export default class List extends React.Component {
+  state = 0;
+
+  emptyFunction = () => {
+    if (this.state === 0) {
+      if (this.props.id === "list-1") {
+        return (
+          <EmptyDiv>
+            It looks like there any tasks left for you to do. Congrats!
+          </EmptyDiv>
+        );
+      }
+      if (this.props.id === "list-2") {
+        return (
+          <EmptyDiv>
+            This is a bit awkward. I'm no snitch but you really should be
+            working.
+          </EmptyDiv>
+        );
+      }
+      if (this.props.id === "list-3") {
+        return (
+          <EmptyDiv>
+            Why is this list empty? You could <i>at least try</i> to get
+            something done.
+          </EmptyDiv>
+        );
+      }
+      if (this.props.id === "list-4") {
+        return (
+          <div>
+            <EmptyDiv>
+              If you can't wrap your head around a task or it simply doesn't
+              work, just drop it here and one of your supervisors will come and
+              help you.
+            </EmptyDiv>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <EmptyDiv> You monumental fuck-up.</EmptyDiv>
+          </div>
+        );
+      }
+    }
+  };
+
   render() {
     let numarOite = this.props.id.replace(/^\D+/g, "");
     let titluLista = "#" + numarOite + ": " + this.props.title + " ";
@@ -63,6 +121,7 @@ export default class List extends React.Component {
                     //console.log("cardul " + IDCard + " are indexul " + index + " in lista " + this.props.id + ".");
                     UNIDMS = carduri.id;
                     UNTITLUMS = carduri.title;
+                    this.state++;
                   }
                 });
                 return (
@@ -74,6 +133,7 @@ export default class List extends React.Component {
                   />
                 );
               })}
+              {this.emptyFunction()}
               {provided.placeholder}
             </CardsDiv>
 
