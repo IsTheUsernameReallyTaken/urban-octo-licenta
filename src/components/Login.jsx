fa3c5645750352879ff5d159248e898205094818
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-05-12 20:49:47 +0300

    Well it looks great but it has no functionality yet

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  position: relative;
-  height: 100px;
-`;
+import { TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
 
 const Wrapper = styled.div`
   width: 600px;
-  height: 300px;
+  height: 275px;
 
   margin: 20px;
   padding: 20px;
@@ -16,7 +15,7 @@ const Wrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
 
-  background: darksalmon;
+  background: GhostWhite;
   text-align: center;
 
   border-radius: 7px;
@@ -24,16 +23,70 @@ const Wrapper = styled.div`
   border-width: thin;
 `;
 
-export default class Login extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Wrapper>
-          <div>Hello there! Welcome to our (MINE) tiny little app.</div>
+const TitleDiv = styled.div`
+  margin-bottom: 20px;
+  font-size: 1.2em;
+`;
 
-          <form></form>
-        </Wrapper>
-      </Container>
-    );
-  }
+const TextFieldDiv = styled.div`
+  margin-top: 10px;
+  padding: 5px;
+`;
+
+const SubmitDiv = styled.div`
+  margin-top: 20px;
+  padding: 5px;
+`;
+
+const initialCredentials = {
+  username: "",
+  password: "",
+};
+
+export default function Login() {
+  const [credentials, setCredentials] = useState(initialCredentials);
+
+  function handleSextChangeHaha() {}
+
+  return (
+    <Wrapper>
+      <TitleDiv>Hello there! Welcome to our (MINE) tiny little app.</TitleDiv>
+      <form>
+        <TextFieldDiv>
+          <TextField
+            variant="outlined"
+            label="Username"
+            name="username"
+            value={credentials.username}
+            onChange={handleSextChangeHaha}
+          />
+        </TextFieldDiv>
+
+        <TextFieldDiv>
+          <TextField
+            variant="outlined"
+            label="Password"
+            name="password"
+            value={credentials.password}
+            onChange={handleSextChangeHaha}
+          />
+        </TextFieldDiv>
+
+        <SubmitDiv>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={function () {
+              console.log("AM APASAT BUTONUL");
+              console.log("Textul din Username: " + credentials.username);
+              console.log("Textul din Password: " + credentials.password);
+            }}
+          >
+            LOGIN
+          </Button>
+        </SubmitDiv>
+      </form>
+    </Wrapper>
+  );
 }
