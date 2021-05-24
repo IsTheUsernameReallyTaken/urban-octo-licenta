1bd98ae5956644d6b0b545c9525b9aba3738731e
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-05-24 16:40:17 +0300

    Pop-up now kinda looks like login! HOORAY!

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { TextField, Button } from "@material-ui/core";
+
 const WrapperDiv = styled.div`
   position: fixed;
   top: 0;
@@ -12,23 +14,76 @@ const WrapperDiv = styled.div`
   background: white;
 
   padding: 20px;
+`;
 
+const TitleDiv = styled.div`
+  margin-bottom: 20px;
+  font-size: 1.2em;
+`;
+
+const TextFieldDiv = styled.div`
+  margin-top: 10px;
+  padding: 5px;
+`;
+
+const ButtonsFlexDiv = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
+`;
+
+const SubmitDiv = styled.div`
+  margin-top: 20px;
+  padding: 5px;
 `;
 
 export default function PopUp(props) {
   return props.show ? (
     <WrapperDiv>
-      <div>Test de popup</div>
-      <button
-        onClick={() => {
-          props.showFunction(false);
-        }}
-      >
-        close me
-      </button>
+      <TitleDiv>
+        <div>Insert a title for the card you'd like to add</div>
+      </TitleDiv>
+      <form>
+        <TextFieldDiv>
+          <TextField
+            id="cardTitleField"
+            variant="outlined"
+            label="Card Title"
+          />
+        </TextFieldDiv>
+
+        <ButtonsFlexDiv>
+          <SubmitDiv>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={() => {
+                const cardTitle =
+                  document.getElementById("cardTitleField").value;
+
+                console.log(
+                  "HOORAY, adaugam un card cu titlul " + cardTitle + "."
+                );
+              }}
+            >
+              ADD CARD
+            </Button>
+          </SubmitDiv>
+
+          <SubmitDiv>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={() => {
+                props.showFunction(false);
+              }}
+            >
+              DISCARD
+            </Button>
+          </SubmitDiv>
+        </ButtonsFlexDiv>
+      </form>
     </WrapperDiv>
   ) : (
     <div />
--- src/components/WelcomeCard.jsx
+++ src/components/WelcomeCard.jsx
@@ -8,12 +8,22 @@ const Wrapper = styled.section`
   border-radius: 10px;
   border-style: solid;
   border-width: thin;
+
+  position: relative;
 `;
 
 const Title = styled.h1`
   font-size: 1.7em;
   text-align: center;
   color: palevioletred;
+`;
+
+const ButtonDiv = styled.button`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 10px;
+  padding: 4px;
 `;
 
 export default function WelcomeCard(props) {
@@ -23,14 +33,13 @@ export default function WelcomeCard(props) {
   return (
     <Wrapper>
       <Title>{welcome}</Title>
-      <button
+      <ButtonDiv
         onClick={() => {
           setButtonShow(true);
-          console.log("Something");
         }}
       >
         Show puppy up
-      </button>
+      </ButtonDiv>
       <PopUp show={buttonShow} showFunction={setButtonShow} />
     </Wrapper>
   );
