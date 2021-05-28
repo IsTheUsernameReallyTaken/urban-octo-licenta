1dd916f64fa486bd4a30863f9d1b263c83517b68
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-05-28 13:38:45 +0300

    Did some Review thingies

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,24 +3,52 @@ import styled from "styled-components";
 
 import firebase from "../firebase";
 import "firebase/firestore";
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 
-const Wrapper = styled.div`
+const Background = styled.div`
   position: fixed;
   width: 100%;
   height: 100vh;
 
-  background: white;
+  top: 0;
+  left: 0;
 
+  background: white;
+`;
+
+const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const MarginDiv = styled.div`
+  margin-top: 10px;
+  padding: 5px;
+`;
+
+const RowFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ColumnFlex = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default function Review(props) {
   const [users, setUsers] = useState([]);
   const [lists, setLists] = useState([]);
   const [cards, setCards] = useState([]);
+
+  const [selectValue, setSelectValue] = useState("by-progress");
 
   const refUsers = firebase.firestore().collection("usernames");
   const refLists = firebase.firestore().collection("lists");
@@ -63,17 +91,45 @@ export default function Review(props) {
   }, []);
 
   return props.show ? (
-    <Wrapper>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => {
-          props.showFunction(false);
-        }}
-      >
-        CLOSE ME
-      </Button>
-    </Wrapper>
+    <Background>
+      <MarginDiv>
+        <FormControl style={{ minWidth: "200px" }} variant="outlined">
+          <InputLabel>Review Type</InputLabel>
+          <Select
+            label="Review Type"
+            value={selectValue}
+            onChange={(event) => {
+              //console.log("Ati ales valoarea ");
+              //console.log(event.target.value);
+              setSelectValue(event.target.value);
+            }}
+          >
+            <MenuItem value={"by-progress"}>By progress</MenuItem>
+            <MenuItem value={"by-dept"}>By department</MenuItem>
+          </Select>
+        </FormControl>
+      </MarginDiv>
+      <MarginDiv>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            props.showFunction(false);
+          }}
+        >
+          CLOSE ME
+        </Button>
+      </MarginDiv>
+      <Wrapper>
+        {selectValue === "by-progress" ? (
+          <h1>This is a review by progress.</h1>
+        ) : selectValue === "by-dept" ? (
+          <h1>This is a review by department</h1>
+        ) : (
+          <div />
+        )}
+      </Wrapper>
+    </Background>
   ) : (
     <div />
   );
