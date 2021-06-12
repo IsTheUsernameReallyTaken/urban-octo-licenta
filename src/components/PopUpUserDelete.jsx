3c6bf2153587559258e90f7ad0e124bb6b09eb8c
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-06-12 17:46:09 +0300

    Warning for deleting users - can now move cards

--- src/components/PopUpUserDelete.jsx
+++ src/components/PopUpUserDelete.jsx
@@ -55,10 +55,57 @@ const SubmitDiv = styled.div`
   padding: 5px;
 `;
 
+const SmallWrapper = styled.div`
+  position: fixed;
+  width: 40%;
+  height: 40vh;
+
+  top: 30%;
+  left: 30%;
+
+  border-radius: 7px;
+  border-style: solid;
+  border-width: thin;
+
+  background: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SmallTitleDiv = styled.div`
+  margin-bottom: 7px;
+  font-size: 1.4em;
+  padding: 40px;
+
+  text-align: center;
+`;
+
+const CenterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ColumnFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const RowFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 export default function PopUpUserDelete(props) {
   const [users, setUsers] = useState([]);
+  const [lists, setLists] = useState([]);
+  const [cards, setCards] = useState([]);
 
   const refUsers = firebase.firestore().collection("usernames");
+  const refLists = firebase.firestore().collection("lists");
+  const refCards = firebase.firestore().collection("cards");
 
   function getUsers() {
     refUsers.onSnapshot((querySnapshot) => {
@@ -70,12 +117,66 @@ export default function PopUpUserDelete(props) {
     });
   }
 
+  function getLists() {
+    refLists.onSnapshot((querySnapshot) => {
+      const listItems = [];
+      querySnapshot.forEach((document) => {
+        listItems.push(document.data());
+      });
+      setLists(listItems);
+    });
+  }
+
+  function getCards() {
+    refCards.onSnapshot((querySnapshot) => {
+      const cardItems = [];
+      querySnapshot.forEach((document) => {
+        cardItems.push(document.data());
+      });
+      setCards(cardItems);
+    });
+  }
+
+  function updateList(updatedList) {
+    refLists
+      .doc(updatedList.id)
+      .update(updatedList)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  function updateCard(updatedCard) {
+    refCards
+      .doc(updatedCard.id)
+      .update(updatedCard)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  function deleteCard(id) {
+    refCards
+      .doc(id)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   useEffect(() => {
     getUsers();
+    getLists();
+    getCards();
   }, []);
 
   const [selectedUser, setSelectedUser] = useState("");
   const [selectError, setSelectError] = useState(false);
+
+  const [warningShow, setWarningShow] = useState(false);
+
+  const [username1, setUsername1] = useState("");
+  const [hasCards1, setHasCards1] = useState("");
 
   function deleteUser(id) {
     refUsers
@@ -92,8 +193,120 @@ export default function PopUpUserDelete(props) {
       return;
     }
 
+    let username;
+
+    users.forEach((useri) => {
+      if (useri.id === selectedUser) {
+        username = useri.username;
+      }
+    });
+
+    let hasCards = [];
+    cards.forEach((carduri) => {
+      if (carduri.by === username) {
+        hasCards[hasCards.length] = carduri.id;
+      }
+    });
+
+    if (hasCards.length === 0) {
+      deleteUser(selectedUser);
+      props.showFunction(false);
+    } else {
+      setUsername1(username);
+      setHasCards1(hasCards);
+      setWarningShow(true);
+    }
+
+    console.log(hasCards);
+    return;
+  }
+
+  function onWarningMoveCards() {
+    hasCards1.forEach((cardIDs) => {
+      cards.forEach((carduri) => {
+        if (carduri.id === cardIDs) {
+          // console.log(carduri.title);
+          let cardToMove = {
+            id: carduri.id,
+            title: carduri.title,
+            department: carduri.department,
+            startTime: "",
+            endTime: "",
+            problemStart: "",
+            problemEnd: "",
+            by: "",
+          };
+          // console.log(cardToMove);
+          updateCard(cardToMove);
+        }
+      });
+
+      lists.forEach((listele) => {
+        if (listele.hasCards.includes(cardIDs)) {
+          // console.log(listele.id);
+          // console.log(listele.hasCards);
+
+          // console.log(listele.hasCards.indexOf(cardIDs));
+
+          let oldHasCards, newHasCards;
+
+          oldHasCards = listele.hasCards;
+          newHasCards = listele.hasCards;
+          newHasCards.splice(listele.hasCards.indexOf(cardIDs), 1);
+
+          // console.log("pentru " + listele.id);
+
+          // console.log(oldHasCards);
+          // console.log(newHasCards);
+
+          let listToUpdate = {
+            id: listele.id,
+            hasCards: newHasCards,
+          };
+
+          // console.log(listToUpdate);
+          updateList(listToUpdate);
+        }
+      });
+    });
+
+    let newHasCards;
+
+    newHasCards = lists[0].hasCards.concat(hasCards1);
+
+    let updatedList = {
+      id: lists[0].id,
+      hasCards: newHasCards,
+    };
+
+    updateList(updatedList);
     deleteUser(selectedUser);
+
+    setWarningShow(false);
+    setSelectError(false);
+    setSelectedUser("");
     props.showFunction(false);
+  }
+
+  function onWarningDeleteCards() {
+    // let theCards1 = [];
+    // lists.forEach((listele) => {
+    //   if (listele.id === selectedList) {
+    //     theCards1 = listele.hasCards;
+    //   }
+    // });
+    // cards.forEach((carduri1) => {
+    //   theCards1.forEach((carduriID) => {
+    //     if (carduri1.id === carduriID) {
+    //       deleteCard(carduri1.id);
+    //     }
+    //   });
+    // });
+    // deleteList(selectedList);
+    // setWarningShow(false);
+    // setSelectError(false);
+    // setSelectedList("");
+    // props.showFunction(false);
   }
 
   return props.show ? (
@@ -109,7 +322,7 @@ export default function PopUpUserDelete(props) {
             variant="outlined"
             error={selectError}
           >
-            <InputLabel>Users</InputLabel>
+            <InputLabel>{warningShow ? "" : "Users"}</InputLabel>
             <Select
               label="Users"
               disabled={users.length === 0}
@@ -168,6 +381,64 @@ export default function PopUpUserDelete(props) {
           </SubmitDiv>
         </ButtonsFlexDiv>
       </form>
+
+      {warningShow ? (
+        <SmallWrapper>
+          <ColumnFlex>
+            <SmallTitleDiv>
+              You are about to delete a user that has cards associated with
+              them.
+            </SmallTitleDiv>
+            <CenterDiv>
+              <RowFlex>
+                <SubmitDiv>
+                  <Button
+                    style={{ background: "black", color: "white" }}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    onClick={() => {
+                      onWarningMoveCards();
+                    }}
+                  >
+                    Move cards
+                  </Button>
+                </SubmitDiv>
+
+                <SubmitDiv>
+                  <Button
+                    style={{ background: "black", color: "white" }}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    onClick={() => {
+                      onWarningDeleteCards();
+                    }}
+                  >
+                    Delete cards
+                  </Button>
+                </SubmitDiv>
+
+                <SubmitDiv>
+                  <Button
+                    style={{ background: "black", color: "white" }}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    onClick={() => {
+                      setWarningShow(false);
+                    }}
+                  >
+                    Back
+                  </Button>
+                </SubmitDiv>
+              </RowFlex>
+            </CenterDiv>
+          </ColumnFlex>
+        </SmallWrapper>
+      ) : (
+        <div />
+      )}
     </WrapperDiv>
   ) : (
     <div />
