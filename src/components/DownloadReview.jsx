d911fb1a4e33a350da1585742cf019eceb1e8bc3
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-06-21 19:10:36 +0300

    Some progress once again

--- src/components/DownloadReview.jsx
+++ src/components/DownloadReview.jsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
-
-import firebase from "../firebase";
-import "firebase/firestore";
-
 export default function DownloadReview(username, users, lists, cards) {
   let reviews = "Reviews \tas of " + new Date().toLocaleString("en-GB");
   reviews = reviews + "\n\t\tdownloaded by " + username;
 
-  console.log(users);
+  reviews = reviews + "\n\n - BY PROGRESS - ";
+
+  let completed = "\n\tCompleted cards: ";
+
+  lists[2].hasCards.length === 0
+    ? (completed = completed + "no cards here")
+    : lists[2].hasCards.forEach((carduri) => {
+        completed = completed + String(carduri.id);
+      });
+
+  reviews = reviews + completed;
 
   return reviews;
 }
