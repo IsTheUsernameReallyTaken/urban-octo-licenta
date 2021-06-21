25bcf9ba795423a4bf9dc27190e15835c3909819
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2021-06-21 15:27:11 +0300

    Actually now it's solved

--- src/components/DownloadReview.jsx
+++ src/components/DownloadReview.jsx
@@ -4,26 +4,27 @@ import firebase from "../firebase";
 import "firebase/firestore";
 
 export default function DownloadReview(username) {
+  const users = [];
+  const lists = [];
+  const cards = [];
+
   const refLists = firebase.firestore().collection("lists");
   const refCards = firebase.firestore().collection("cards");
   const refUsers = firebase.firestore().collection("usernames");
 
   refLists.onSnapshot((querySnapshot) => {
-    const lists = [];
     querySnapshot.forEach((document) => {
       lists.push(document.data());
     });
   });
 
   refCards.onSnapshot((querySnapshot) => {
-    const cards = [];
     querySnapshot.forEach((document) => {
       cards.push(document.data());
     });
   });
 
   refUsers.onSnapshot((querySnapshot) => {
-    const users = [];
     querySnapshot.forEach((document) => {
       users.push(document.data());
     });
